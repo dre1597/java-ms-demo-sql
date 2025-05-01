c0bc2e6e0777a0612f609e86012069daf884a515
André Pereira <andrepereira97@yahoo.com>
2025-05-01 20:57:01 -0300

    :tada: start project

---
+++ src/main/java/org/example/javamsdemosql/JavaMsDemoSqlApplication.java
@@ -0,0 +1,13 @@
+package org.example.javamsdemosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsDemoSqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaMsDemoSqlApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/example/javamsdemosql/JavaMsDemoSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javamsdemosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMsDemoSqlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
