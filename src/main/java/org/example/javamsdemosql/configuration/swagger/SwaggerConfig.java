4383909fdf7d243507723ea7518f3fadbb8ae717
André Pereira <andrepereira97@yahoo.com>
2025-05-01 22:17:23 -0300

    :sparkles: implement swagger, postgres, rabbitmq

---
+++ src/main/java/org/example/javamsdemosql/configuration/swagger/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package org.example.javamsdemosql.configuration.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiOAuthProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springdoc.webmvc.ui.SwaggerIndexTransformer;
+import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI openAPI() {
+    final var info = new Info()
+        .title("MS demo API")
+        .description("MS demo using Spring")
+        .version("v0.0.1");
+
+    return new OpenAPI()
+        .info(info);
+  }
+
+  @Bean
+  public SwaggerIndexTransformer swaggerIndexTransformer(
+      final SwaggerUiConfigProperties swaggerUiConfig,
+      final SwaggerUiOAuthProperties swaggerUiOAuthProperties,
+      final SwaggerWelcomeCommon swaggerWelcomeCommon,
+      final ObjectMapperProvider objectMapperProvider
+  ) {
+    return new SwaggerCustomCssInjector(swaggerUiConfig, swaggerUiOAuthProperties, swaggerWelcomeCommon, objectMapperProvider);
+  }
+}
---
+++ src/main/java/org/example/javamsdemosql/configuration/swagger/SwaggerCustomCssInjector.java
@@ -0,0 +1,50 @@
+package org.example.javamsdemosql.configuration.swagger;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiOAuthProperties;
+import org.springdoc.core.providers.ObjectMapperProvider;
+import org.springdoc.webmvc.ui.SwaggerIndexPageTransformer;
+import org.springdoc.webmvc.ui.SwaggerWelcomeCommon;
+import org.springframework.core.io.Resource;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.resource.ResourceTransformerChain;
+import org.springframework.web.servlet.resource.TransformedResource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class SwaggerCustomCssInjector extends SwaggerIndexPageTransformer {
+  public SwaggerCustomCssInjector(
+      final SwaggerUiConfigProperties swaggerUiConfig,
+      final SwaggerUiOAuthProperties swaggerUiOAuthProperties,
+      final SwaggerWelcomeCommon swaggerWelcomeCommon,
+      final ObjectMapperProvider objectMapperProvider) {
+    super(swaggerUiConfig, swaggerUiOAuthProperties, swaggerWelcomeCommon, objectMapperProvider);
+  }
+
+  @Override
+  public @NonNull Resource transform(
+      @NonNull final HttpServletRequest request,
+      @NonNull final Resource resource,
+      @NonNull final ResourceTransformerChain transformer
+  ) throws IOException {
+    if ("index.html".equals(resource.getFilename())) {
+      try (InputStream in = resource.getInputStream();
+           BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+        final var html = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        final var transformedHtml = injectCss(html);
+        return new TransformedResource(resource, transformedHtml.getBytes());
+      }
+    }
+    return super.transform(request, resource, transformer);
+  }
+
+  private String injectCss(String html) {
+    final var cssPath = "/static/swagger-theme.css";
+    return html.replace("</head>", "<link rel=\"stylesheet\" type=\"text/css\" href=\"" + cssPath + "\" /></head>");
+  }
+}
