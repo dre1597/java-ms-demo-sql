eafcb88b318a904429ed2e7409f80897e7e2bc7b
André Pereira <andrepereira97@yahoo.com>
2025-05-04 23:42:23 -0300

    :sparkles: simplify endpoints

--- src/main/java/org/example/javamsdemosql/controllers/MessageController.java
+++ src/main/java/org/example/javamsdemosql/controllers/MessageController.java
@@ -2,11 +2,12 @@ package org.example.javamsdemosql.controllers;
 
 import org.example.javamsdemosql.dto.MessageDto;
 import org.example.javamsdemosql.entities.MessageEntity;
-import org.example.javamsdemosql.enums.MessageStatus;
 import org.example.javamsdemosql.services.FindAllMessagesUseCase;
-import org.example.javamsdemosql.services.FindByStatusUseCase;
 import org.example.javamsdemosql.services.SendMessageUseCase;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Objects;
@@ -15,16 +16,13 @@ import java.util.Objects;
 public class MessageController {
   private final SendMessageUseCase sendMessageUseCase;
   private final FindAllMessagesUseCase findAllMessagesUseCase;
-  private final FindByStatusUseCase findByStatusUseCase;
 
   public MessageController(
       final SendMessageUseCase sendMessageUseCase,
-      final FindAllMessagesUseCase findAllMessagesUseCase,
-      final FindByStatusUseCase findByStatusUseCase
+      final FindAllMessagesUseCase findAllMessagesUseCase
   ) {
     this.sendMessageUseCase = Objects.requireNonNull(sendMessageUseCase);
     this.findAllMessagesUseCase = Objects.requireNonNull(findAllMessagesUseCase);
-    this.findByStatusUseCase = Objects.requireNonNull(findByStatusUseCase);
   }
 
   @PostMapping("/send")
@@ -35,10 +33,5 @@ public class MessageController {
   @GetMapping()
   public List<MessageEntity> findAllMessages() {
     return this.findAllMessagesUseCase.execute();
-  }
-
-  @GetMapping("/status/{status}")
-  public List<MessageEntity> findByStatus(@PathVariable final String status) {
-    return this.findByStatusUseCase.execute(MessageStatus.valueOf(status));
   }
 }
--- src/test/java/org/example/javamsdemosql/entities/MessageEntityTest.java
+++ src/test/java/org/example/javamsdemosql/entities/MessageEntityTest.java
@@ -1,59 +1,44 @@
 package org.example.javamsdemosql.entities;
 
-import org.example.javamsdemosql.enums.MessageStatus;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class MessageEntityTest {
   @Test
   void shouldCreateMessageEntity() {
     final var id = UUID.randomUUID();
-    final var now = LocalDateTime.now();
 
     final var jpaEntity = new MessageEntity(
         id,
         "any_title",
-        "any_content",
-        MessageStatus.PROCESSED,
-        now,
-        now,
-        0
+        "any_content"
     );
 
     assertEquals(id, jpaEntity.getId());
     assertEquals("any_title", jpaEntity.getTitle());
     assertEquals("any_content", jpaEntity.getContent());
-    assertEquals(MessageStatus.PROCESSED, jpaEntity.getStatus());
-    assertEquals(now, jpaEntity.getCreatedAt());
-    assertEquals(now, jpaEntity.getProcessedAt());
-    assertEquals(0, jpaEntity.getRetryAttempts());
   }
 
   @Test
   void shouldSetAndGetFieldsCorrectly() {
     final var id = UUID.randomUUID();
-    final var now = LocalDateTime.now();
-
     final var jpaEntity = new MessageEntity();
 
     jpaEntity.setId(id);
     jpaEntity.setTitle("any_title");
     jpaEntity.setContent("any_content");
-    jpaEntity.setStatus(MessageStatus.PROCESSED);
-    jpaEntity.setCreatedAt(now);
-    jpaEntity.setProcessedAt(now);
-    jpaEntity.setRetryAttempts(0);
+    jpaEntity.setCreatedAt(LocalDateTime.now());
+    jpaEntity.setUpdatedAt(LocalDateTime.now());
 
     assertEquals(id, jpaEntity.getId());
     assertEquals("any_title", jpaEntity.getTitle());
     assertEquals("any_content", jpaEntity.getContent());
-    assertEquals(MessageStatus.PROCESSED, jpaEntity.getStatus());
-    assertEquals(now, jpaEntity.getCreatedAt());
-    assertEquals(now, jpaEntity.getProcessedAt());
-    assertEquals(0, jpaEntity.getRetryAttempts());
+    assertNotNull(jpaEntity.getCreatedAt());
+    assertNotNull(jpaEntity.getUpdatedAt());
   }
 }
